f0a258c82820c35be525e79c7f1749cd7c8df916
Bahadır Bülbül <bulbulbahadir1@gmail.com>
2021-11-21 00:16:57 +0300

    Report ve User microservice'leri için Test projeleri oluşturuldu.

---
+++ Tests/Phonebook.Services.User.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Phonebook.Services.User.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
